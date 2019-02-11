6066208eccdef06ab2967ed60a471bc5025c7217
nuriath <mnuriath28@gmail.com>
2019-02-11 17:31:50 +0200

    add TestUser in contact_test.py file

--- contact.py
+++ contact.py
@@ -1,17 +1,65 @@
 class User:
     """
-    Class that generates new instances of contacts.
+    Class that generates new instances of contacts
     """
-    contact_list = [] 
+
+    pass
+    contact_list = [] # Empty contact list
 
     def __init__(self,email,username,password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            email: New contact email.
+            username : New contact username.
+            password: New contact password.
+        '''
+           # docstring removed for simplicity
+
 
         self.email = email
         self.username = username
         self.password = password
+       
+        contact_list = [] # Empty contact list
         
-    def create_contact(email,username,password):
+        # Init method up here
+    def save_contact(self):
 
-        new_contact = User(email,username,password)
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
 
+        User.contact_list.append(self)
 
+    def delete_contact(self):
+
+        '''
+        delete_contact method deletes a saved contact from the contact_list
+        '''
+
+        User.contact_list.remove(self)
+
+    @classmethod
+    def find_by_username(cls,username):
+        '''
+        Method that takes in a username and returns a contact that matches that username.
+
+        Args:
+            username: username to search for
+        Returns :
+            Contact of person that matches the username.
+        '''
+
+        for contact in cls.contact_list:
+            if contact.username == username:
+                return contact
+
+    @classmethod
+    def display_contacts(cls):
+        '''
+        method that returns the contact list
+        '''
+        return cls.contact_list       
--- contact_test.py
+++ contact_test.py
@@ -9,14 +9,13 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
-
     # Items up here .......
 
     def setUp(self):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_contact = User("Queen@gmail.com","kingdom","jquery2") # create contact object
+        self.new_contact = User("queen@gmail.com","kingdom","jquery2") # create contact object
 
 
     def test_init(self):
@@ -24,10 +23,10 @@ class TestUser(unittest.TestCase):
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_contact.email,"Queen@gmail.com")
+        self.assertEqual(self.new_contact.email,"queen@gmail.com")
         self.assertEqual(self.new_contact.username,"kingdom")
-        self.assertEqual(self.new_contact.phone_number,"jquery2")
-    
+        self.assertEqual(self.new_contact.password,"jquery2")
+        
     def test_save_contact(self):
         '''
         test_save_contact test case to test if the contact object is saved into
@@ -35,3 +34,28 @@ class TestUser(unittest.TestCase):
         '''
         self.new_contact.save_contact() # saving the new contact
         self.assertEqual(len(User.contact_list),1)
+
+# Items up here...
+
+# setup and class creation up here
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+    User.contact_list = []
+# other test cases here
+    def test_save_multiple_contact(self):
+            '''
+            test_save_multiple_contact to check if we can save multiple contact
+            objects to our contact_list
+            '''
+            self.new_contact.save_contact()
+            test_contact = User("queen@gmail.com","kingdom","jquery2") # new contact
+            test_contact.save_contact()
+            self.assertEqual(len(User.contact_list),2)
+
+            # More tests above
+    
+
+if __name__ == '__main__':
+    unittest.main()
